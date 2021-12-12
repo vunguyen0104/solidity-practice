8c783de58248a33dd76b2680ace9f6495bc7f873
linhloan <linhloantran10121994@gmail.com>
2021-12-11 20:07:41 -0500

    Added comment

--- demo.sol
+++ demo.sol
@@ -11,6 +11,7 @@ contract SimpleStorage {
     function get() public view returns (uint256) {
         // added comment here by vunguyen0104
         // added comment here by linhloan
+        // added comment 3 by linhloan
         return storedData;
     }
 }
